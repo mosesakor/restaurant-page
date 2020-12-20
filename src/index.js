a2b33064e7b1091774eded9873d11cc65ea139c8
Moses Akor <gabrielmosesakor@gmail.com>
2020-12-20 21:47:04 +0100

    finished with most of the functionalities

--- src/index.js
+++ src/index.js
@@ -49,13 +49,19 @@ const nav = () => {
   }
 
   const contact_nav = () => {
-    const contact = document.getElementById("home");
+    const contact = document.getElementById("contact");
     contact.onclick = function(e) {
       resetPage();
       render_contact();
     }
   }
-  return {render};
+
+  const addEventListeners = () => {
+    home_nav();
+    menu_nav();
+    contact_nav();
+  }
+  return {render, addEventListeners};
 }
 
 const resetPage = () => {
@@ -64,13 +70,57 @@ const resetPage = () => {
 }
 
 const render_home = () => {
-  const content = document.getElementById("content")
+  const content = document.getElementById("content");
   const description = document.createElement("p");
   description.textContent = "Everything we do is about you. From chefs who create exciting new flavors, to crew members who know exactly how you want your drink – we prioritize what you need to get you on your way. We strive to keep you at your best, and we remain loyal to you, your tastes and your time."
   description.classList.add("description");
   content.appendChild(description);
 }
 
+const render_contact = () => {
+  const content = document.getElementById("content");
+  const info = document.createElement("p")
+  const contact_info = document.createElement("ul");
+  const phone = document.createElement("li");
+  const email = document.createElement("li");
+  const location = document.createElement("li");
+
+  info.textContent = "Come eat at our place, you wouldn't regret it"
+  phone.textContent = "Tel: 07911 123456"
+  email.textContent = "Email: Jollyeats@jolly.zz"
+  location.textContent = "Location: 10 Downing street"
+
+
+  contact_info.appendChild(phone);
+  contact_info.appendChild(email);
+  contact_info.appendChild(location);
+
+  content.appendChild(info);
+  content.appendChild(contact_info);
+}
+
+const render_menu = () => {
+  const content = document.getElementById("content");
+  const menu = document.createElement("ul");
+  const item1 = document.createElement("li");
+  const item2 = document.createElement("li");
+  const item3 = document.createElement("li");
+  const item4 = document.createElement("li");
+
+  item1.textContent = "Proscuitto Salmon     $15";
+  item2.textContent = "Chinese Fried Rice    £12";
+  item3.textContent = "Butter Chicken Rice   £15";
+  item4.textContent = "Briesd Beef Rice      £12";
+
+  menu.appendChild(item1);
+  menu.appendChild(item2);
+  menu.appendChild(item3);
+  menu.appendChild(item4);
+
+  content.appendChild(menu);
+}
+
 const test = nav()
 test.render();
+test.addEventListeners();
 render_home();
